9b9267709f90097ee76dc224c9483bc79b879597
Sebastian Schindler <Sebastian.schindler@capgemini.com>
2017-07-14 08:23:49 +0200

    Conflict?

--- src/main/java/MyTestClass.java
+++ src/main/java/MyTestClass.java
@@ -1,19 +1,21 @@
 import java.awt.*;
+
 public class MyTestClass {
 
-	
-	String _TEST;
-	
-	void INVALID_METHOD_NAME() {
-		
-	}
-	
-	
+  String _TEST;
 
+  void INVALID_METHOD_NAME() {
 
-	public static void main(String[] args) {
-		
-		String s = "test"; Integer i = 1;
-	}
+  }
+
+  public static void main(String[] args) {
+
+    String s = "test";
+    Integer i = 1;
+
+    if (true) {
+      String s2;
+    }
+  }
 
 }
